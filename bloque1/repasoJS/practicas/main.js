dc54f4664da2fb06aa32188e54af4a861bdbf00d
jalilmanuel25 <jalilmanuel25@gmail.com>
2020-09-21 21:54:18 -0500

    Se añade la practica de la calculadora

---
+++ bloque1/repasoJS/practicas/main.js
@@ -0,0 +1,31 @@
+function calcular(){
+    let numero2 = parseFloat(document.querySelector("#numero2").value);
+    let numero1 = parseFloat(document.querySelector("#numero1").value);
+    let operacion = document.querySelector("#operacion").value;
+    let p_resultado = document.querySelector("#resultado");
+    let resultado, operador;
+
+    switch (operacion){
+        case "suma":
+            resultado = numero1 + numero2;
+            operador = " + ";
+        break;
+
+        case "resta":
+            resultado = numero1 - numero2;
+            operador = " - ";
+        break;
+
+        case "multiplicacion":
+            resultado = numero1 * numero2;
+            operador = " x ";
+        break;
+
+        case "division":
+            resultado = numero1 / numero2;
+            operador = " / ";
+        break;
+    }
+    
+    p_resultado.textContent = `${numero1} ${operador} ${numero2} = ${resultado}`;
+}
